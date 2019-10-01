b77eb067ac3d4587d71dd0a2bac054363aec5685
Leigh Rogers <leighrogers@Leighs-MacBook-Pro.local>
2019-10-01 15:31:00 -0500

    added details page and detail delete button on Owners and added Admit button functionality on Animals

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react'
 import Home from './home/Home'
 import AnimalList from './animal/AnimalList'
 import EmployeeList from './employee/EmployeeList'
-//only include these once they are built - previous practice exercise
 import LocationList from './location/LocationList'
 import OwnerList from './owner/OwnerList'
 import AnimalDetail from './animal/AnimalDetail'
 import LocationDetail from './location/LocationDetail'
 import EmployeeDetail from './employee/EmployeeDetail'
 import OwnerDetail from './owner/OwnerDetail'
+import AnimalForm from './animal/AnimalForm'
 
 class ApplicationViews extends Component {
 
@@ -21,26 +21,32 @@ class ApplicationViews extends Component {
           }} />
           {/* Make sure you add the `exact` attribute here */}
           <Route exact path="/animals" render={(props) => {
-            return <AnimalList />
+            return <AnimalList {...props}/>
             }} />
           <Route path="/animals/:animalId(\d+)" render={(props) => {
-            // Pass the animalId to the AnimalDetailComponent
             return <AnimalDetail animalId={parseInt(props.match.params.animalId)} {...props}/>
             }} />
+          <Route path="/animals/new" render={(props) => {
+            return <AnimalForm {...props} />
+            }} />
+
+          {/* routes for employees */}
           <Route exact path="/employee" render={(props) => {
             return <EmployeeList />
             }} />
           <Route path="/employee/:employeeId(\d+)" render={(props) => {
-            // Pass the animalId to the AnimalDetailComponent
             return <EmployeeDetail employeeId={parseInt(props.match.params.employeeId)} {...props} />
             }} />
+
+          {/* routes for location */}
           <Route exact path="/location" render={(props) => {
             return <LocationList />
             }} />
           <Route path="/location/:locationId(\d+)" render={(props) => {
-            // Pass the locationId to the LocationDetailComponent
             return <LocationDetail locationId={parseInt(props.match.params.locationId)} {...props}/>
             }} />
+
+          {/* owner routes */}
           <Route exact path="/owner" render={(props) => {
             return <OwnerList />
           }} />
--- src/components/owner/OwnerDetail.js
+++ src/components/owner/OwnerDetail.js
@@ -6,16 +6,25 @@ class OwnerDetail extends Component {
   state = {
       name: "",
       animalId: "",
+      loadingStatus: true,
   }
 
+  handleDelete = () => {
+    //invoke the delete function in AnimalManger and re-direct to the animal list.
+    this.setState({loadingStatus: true})
+    OwnerManager.delete(this.props.ownerId)
+    .then(() => this.props.history.push("/owner"))
+}
+
   componentDidMount(){
-    console.log("AnimalDetail: ComponentDidMount");
+    console.log("OwnerDetail: ComponentDidMount");
     //get(id) from AnimalManager and hang on to the data; put it into state
     OwnerManager.get(this.props.ownerId)
     .then((owner) => {
       this.setState({
         name: owner.name,
-        animalId: owner.animalId
+        animalId: owner.animalId,
+        loadingStatus: false
       });
     });
   }
@@ -29,6 +38,7 @@ class OwnerDetail extends Component {
           </picture> */}
             <h3><span style={{ color: 'darkslategrey' }}>{this.state.name}</span></h3>
             <p>Pet ID: {this.state.animalId}</p>
+            <button type="button" disabled={this.state.loadingStatus} onClick={this.handleDelete}>Remove</button>
         </div>
       </div>
     );
