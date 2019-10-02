82fa57b6f77dc451fee7bc7efb4e9c284ab3d34f
Leigh Rogers <leighrogers@Leighs-MacBook-Pro.local>
2019-10-02 10:09:19 -0500

    added login authentication to animals page

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,4 +1,4 @@
-import { Route } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 import React, { Component } from 'react'
 import Home from './home/Home'
 import AnimalList from './animal/AnimalList'
@@ -11,8 +11,11 @@ import EmployeeDetail from './employee/EmployeeDetail'
 import OwnerDetail from './owner/OwnerDetail'
 import AnimalForm from './animal/AnimalForm'
 import EmployeeForm from './employee/EmployeeForm'
+import Login from './auth/Login'
 
 class ApplicationViews extends Component {
+
+    isAuthenticated = () => localStorage.getItem("credentials") !== null
 
     render() {
       return (
@@ -21,8 +24,12 @@ class ApplicationViews extends Component {
             return <Home />
           }} />
           {/* Make sure you add the `exact` attribute here */}
-          <Route exact path="/animals" render={(props) => {
-            return <AnimalList {...props}/>
+          <Route exact path="/animals" render={props => {
+                if (this.isAuthenticated()) {
+                    return <AnimalList {...props} />
+                } else {
+                    return <Redirect to="/login" />
+                }
             }} />
           <Route path="/animals/:animalId(\d+)" render={(props) => {
             return <AnimalDetail animalId={parseInt(props.match.params.animalId)} {...props}/>
@@ -44,7 +51,7 @@ class ApplicationViews extends Component {
 
           {/* routes for location */}
           <Route exact path="/location" render={(props) => {
-            return <LocationList />
+            return <LocationList {...props}/>
             }} />
           <Route path="/location/:locationId(\d+)" render={(props) => {
             return <LocationDetail locationId={parseInt(props.match.params.locationId)} {...props}/>
@@ -52,12 +59,15 @@ class ApplicationViews extends Component {
 
           {/* owner routes */}
           <Route exact path="/owner" render={(props) => {
-            return <OwnerList />
+            return <OwnerList {...props}/>
           }} />
           <Route path="/owner/:ownerId(\d+)" render={(props) => {
-            // Pass the locationId to the LocationDetailComponent
             return <OwnerDetail ownerId={parseInt(props.match.params.ownerId)} {...props}/>
             }} />
+
+            {/* login route */}
+          <Route path="/login" component={Login} />
+
         </React.Fragment>
       )
     }
